f05bc1c0e2f06b1b8d81fab0f5a3de7349d5cea1
Robson Maia <robsonmvieira@gmail.com>
2020-08-10 18:38:41 -0300

    feat: install styled-components

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-import type { AppProps /*, AppContext */ } from 'next/app'
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
