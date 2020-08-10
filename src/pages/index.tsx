a3d86b7266047f930a3afbeba56fa51bcbfb356a
Robson Maia <robsonmvieira@gmail.com>
2020-08-10 18:41:44 -0300

    feat: add document custom page

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import { Button } from 'reactstrap';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="row">
+        <div className="col-6">
+          <Button color="primary" size="sm">First Button</Button>
+        </div>
+      </div>
+
+
+    </div>
+  )
+}
