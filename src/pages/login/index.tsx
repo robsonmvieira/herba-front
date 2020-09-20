bc31d6b0095272ea0181bc791eb03f4646cc2ffd
Robson Maia <robsonmvieira@gmail.com>
2020-09-20 01:38:44 -0300

    feat: add try catch to login method

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import {
   Container,
-  InputFormLogin,
   LabelLogin,
-  FormLogin,
   BtnLogin,
   Side,
   Title,
@@ -11,26 +9,44 @@ import {
   FormTitle
 } from './styles'
 import Menu from '../../components/Menu'
+import Input from '../../components/Input'
+import { Form } from '@unform/web'
+import api from '../../services/api'
 
+interface userLogin {
+  email: string
+  password: string
+}
 const Login = () => {
+  async function handleSubmit({ email, password }: userLogin) {
+    try {
+      const resp = await api.post('sessions/login', { email, password })
+      const { data } = resp
+      localStorage.setItem('token', data)
+    } catch (error) {
+      if (error.response.status === 401) {
+        console.log('você não tem autorização pra continuar')
+      }
+    }
+  }
+
   return (
     <>
       <Menu />
-
       <Container>
         <Wapper>
           <Side>
             <Title> Life Admin</Title>
           </Side>
           <Side>
-            <FormLogin>
+            <Form onSubmit={handleSubmit}>
               <FormTitle>Login</FormTitle>
-              <LabelLogin>Nome:</LabelLogin>
-              <InputFormLogin />
-              <LabelLogin style={{ marginTop: 25 }}>Email:</LabelLogin>
-              <InputFormLogin />
+              <LabelLogin>Email:</LabelLogin>
+              <Input name="email" />
+              <LabelLogin style={{ marginTop: 25 }}>Senha:</LabelLogin>
+              <Input name="password" type="password" />
               <BtnLogin>Logar</BtnLogin>
-            </FormLogin>
+            </Form>
           </Side>
         </Wapper>
       </Container>
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -144,28 +144,28 @@ export const FormTitle = styled.h2`
   }
 `
 
-export const InputFormLogin = styled.input`
-  @media (max-width: 775px) {
-    width: 80%;
-    height: 48px;
-    /* left: 43px; */
-    /* top: 252px; */
-    background: #282741;
-    color: #eff0f5;
-    border: none;
-  }
-  /* medium */
-  @media (min-width: 776px) {
-    width: 80%;
-    height: 48px;
-    background: #1d1f2f;
-    color: #eff0f5;
-    border: none;
-    display: flex;
-    align-items: center;
-  }
-  /* large */
-`
+// export const InputFormLogin = styled.input`
+//   @media (max-width: 775px) {
+//     width: 80%;
+//     height: 48px;
+//     /* left: 43px; */
+//     /* top: 252px; */
+//     background: #282741;
+//     color: #eff0f5;
+//     border: none;
+//   }
+//   /* medium */
+//   @media (min-width: 776px) {
+//     width: 80%;
+//     height: 48px;
+//     background: #1d1f2f;
+//     color: #eff0f5;
+//     border: none;
+//     display: flex;
+//     align-items: center;
+//   }
+//   /* large */
+// `
 
 export const BtnLogin = styled.button`
   @media (max-width: 775px) {
