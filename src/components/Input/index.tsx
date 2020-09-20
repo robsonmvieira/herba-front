138443c98f1b6320e1f976c954867928de6fddd6
Robson Maia <robsonmvieira@gmail.com>
2020-09-20 11:17:59 -0300

    feat: show catched after submit form

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef } from 'react'
 import { useField } from '@unform/core'
 import { Input as CInput } from './styles'
+import { Toast, ToastBody, ToastHeader } from 'reactstrap'
 
 const Input = ({ name, ...rest }) => {
   const inputRef = useRef(null)
-  const { fieldName, defaultValue, registerField } = useField(name)
+  const { fieldName, defaultValue, registerField, error } = useField(name)
   useEffect(() => {
     registerField({
       name: fieldName,
@@ -14,7 +15,21 @@ const Input = ({ name, ...rest }) => {
       path: 'value'
     })
   }, [fieldName, registerField])
-  return <CInput ref={inputRef} defaultValue={defaultValue} {...rest} />
+  return (
+    <>
+      {error && (
+        <Toast style={{ background: 'red' }}>
+          <ToastHeader style={{ color: '#fff', background: 'red' }}>
+            Erro de validação.
+          </ToastHeader>
+          <ToastBody style={{ color: '#fff' }}>
+            Há erro em um ou mais campos - {error}
+          </ToastBody>
+        </Toast>
+      )}
+      <CInput ref={inputRef} defaultValue={defaultValue} {...rest} />
+    </>
+  )
 }
 
 export default Input
