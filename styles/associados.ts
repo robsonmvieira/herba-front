1cb0f919b2020d8837c1b573c0d3a6320e811a2e
Cleiton Oliveira <cleiton.logistica@outlook.com>
2020-10-06 20:18:32 -0300

    estilo do form associados

--- styles/associados.ts
+++ styles/associados.ts
@@ -1,3 +1,8 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-star;
+  align-items: flex-start;
+`
