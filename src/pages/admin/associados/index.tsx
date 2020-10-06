71847e0b91910ea505e1b3b39292bd6978232718
Cleiton Oliveira <cleiton.logistica@outlook.com>
2020-10-06 20:18:11 -0300

    Criado form de Associados

--- src/pages/admin/associados/index.tsx
+++ src/pages/admin/associados/index.tsx
@@ -1,12 +1,35 @@
-import React from 'react'
-
+import React, { useCallback } from 'react'
 import { Container } from '../../../../styles/associados'
+import Input from '../../../components/Input'
+import { Form } from '@unform/web'
 
-const associados: React.FC = () => {
+const Associados = () => {
+  const handleSubmit = useCallback(() => {
+    console.log('log')
+  }, [])
   return (
     <Container>
       <h2>Associados</h2>
+      <Form onSubmit={handleSubmit}>
+        <label>Nome</label>
+        <Input name="Name" />
+
+        <label>Password</label>
+        <Input name="PassWord" />
+
+        <label>email</label>
+        <Input name="Email" />
+
+        <label>isAdmin</label>
+        <Input type="checkbox" name="IsAdmin" />
+
+        <label>Posição</label>
+        <select name="Roles" />
+
+        <label>Permissão</label>
+        <select name="permissions" />
+      </Form>
     </Container>
   )
 }
-export default associados
+export default Associados
