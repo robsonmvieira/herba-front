f8533da319993665fe5575c2573e21402c8e05ba
Cleiton Oliveira <cleiton.logistica@outlook.com>
2020-10-13 12:39:45 -0300

    :recycle: fechamento do pdd refaturado

--- src/pages/pdd/resumo/index.tsx
+++ src/pages/pdd/resumo/index.tsx
@@ -1,32 +1,185 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
+// import { formatUserName } from '../../utils/formatUserName'
+import { GetServerSideProps, InferGetStaticPropsType } from 'next'
 import {
   Navbar,
   Container,
-  SpanDetalhes,
+  FormList,
+  InputFormList,
+  BtnList,
+  Detalhes,
   ContainerProducts,
   ContainerTotais,
   ContainerVenda,
-  LabelSubtotalDesconto,
-  FormSubtotalDesconto,
+  Label,
+  Descricao,
   ContainerValores,
   LabelValorTotal,
   FormTotal,
+  BtnFormaPagamento,
   BtnFinalizar,
   ContainerBusca,
-  TableHead,
-  ContainerSpan,
-  SpanListTh,
-  SpanListTd,
-  TableRow,
-  DivTabela,
-  ContainerSpanTotais,
-  HTitulo,
+  DropdownItem,
   DropdownMenu,
-  DropdownItem
+  Tr,
+  TrBody,
+  Th,
+  TBody,
+  TabelaBody,
+  BoxCustomName,
+  Whapper,
+  Td,
+  TdCustomImage,
+  WhapperCustomBtn,
+  WhapperCustomNumber,
+  BoxBodyNumbers,
+  InputPdv,
+  BtnPreviosNext,
+  DivBtnPreviusNext,
+  UncontrolledDropdown,
+  UserName,
+  ContainerTh,
+  DropdownToggle,
+  BoxDetalhes,
+  Select
 } from '../styles'
-import { NavbarBrand, UncontrolledDropdown, DropdownToggle } from 'reactstrap'
+import { NavbarBrand  } from 'reactstrap'
+import apiService from '../../../services/apiService'
+import Product from '../../../components/product'
+import { useRouter } from 'next/router'
+// import { nextPagePagination } from '../../utils/nexPagePagination'
+interface Product {
+  id: string
+  name: string
+  sku: string
+  price_suggest: string
+}
 
-const CloseBox = () => {
+export const getServerSideProps: GetServerSideProps = async params => {
+  // const cookie = params.req.headers.cookie
+  // if (!cookie) {
+  //   console.log('Não tenho...')
+  //   params.res.writeHead(302, {
+  //     Location: '/login'
+  //   })
+  //   params.res.statusCode = 302
+  //   params.res.setHeader('Location', '/login')
+  //   params.res.end()
+  //   return { props: {} }
+  // }
+  // if (params.res.statusCode === 401 && !params.req) {
+  //   params.res.statusCode = 302
+  //   params.res.setHeader('Location', '/login')
+  //   // params.res.writeHead(302, {
+  //   //   Location: '/login'
+  //   // })
+  //   params.res.end()
+  //   return { props: {} }
+  // }
+  // if (params.res.statusCode === 401 && params.req) {
+  //   params.res.statusCode = 302
+  //   params.res.setHeader('Location', '/login')
+  //   // params.res.writeHead(302, {
+  //   //   Location: '/login'
+  //   // })
+  //   params.res.end()
+  //   return { props: {} }
+  // }
+  const { pages } = params.query
+  if (pages) {
+    const response = await apiService.get<Product[]>(
+      `/products?pages=${pages}`
+      // {
+      //   // headers: { cookie: cookie || '' }
+      // }
+    )
+    const productsList = response.data
+    // const userName = formatUserName(cookie)
+    return { props: { productsList } }
+  }
+  const response = await apiService.get<Product[]>(
+    `/products?pages=${0}`
+    // {
+    //   // headers: { cookie: cookie || '' }
+    // }
+  )
+  const productsList = response.data
+  // const userName = formatUserName(cookie)
+  return { props: { productsList } }
+}
+
+const ListProducts = ({
+  productsList
+}: InferGetStaticPropsType<typeof getServerSideProps>) => {
+  const router = useRouter()
+
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  const [products, setProducts] = useState(productsList)
+  const [termOfFind, setTermOfFind] = useState('')
+
+  let currentPage = router.query.pages ? Number(router.query.pages) : 1
+
+  // function searchProducHandler(e) {
+
+  // }
+
+  const nextProducts = useCallback(async () => {
+    // verify if current page is 0 or NaN
+    if (currentPage <= 0 || currentPage !== Number(currentPage)) {
+      const response = await apiService.get(`/products?pages=${1}`)
+      currentPage++
+      setProducts(response.data)
+    }
+
+    // verify if current page is equal 1
+    if (currentPage === 1) {
+      const response = await apiService.get(
+        `/products?pages=${currentPage + 1}`
+      )
+      currentPage++
+      setProducts(response.data)
+    } else {
+      // then current page is biggest than one
+      const response = await apiService.get(
+        `/products?pages=${currentPage + 1}`
+      )
+      currentPage++
+      setProducts(response.data)
+    }
+    // nextPagePagination(currentPage, apiService, setProducts)
+  }, [currentPage])
+
+  const previusPage = useCallback(async () => {
+    // verify if current page is one or NaN
+    if (currentPage === 1 || currentPage !== Number(currentPage)) {
+      const response = await apiService.get(`/products?pages=${1}`)
+      // currentPage++
+      setProducts(response.data)
+    } else {
+      // then current page is bigger than one
+      const response = await apiService.get(
+        `/products?pages=${currentPage - 1}`
+      )
+      currentPage--
+      setProducts(response.data)
+    }
+  }, [currentPage])
+
+  const searchProductHandler = useCallback(async termOfFind => {
+    const cacheProducts = products
+    const response = await apiService.get(
+      `/associates/productByLike/?terms=${termOfFind}`
+    )
+    setTermOfFind(termOfFind)
+    setProducts(response.data)
+    if (termOfFind.length === 0) {
+      setProducts(cacheProducts)
+    }
+  }, [])
+
   return (
     <div>
       <Navbar light expand="ml">
@@ -37,11 +190,13 @@ const CloseBox = () => {
         </div>
 
         <UncontrolledDropdown setActiveFromChild>
-          <DropdownToggle tag="a" className="nav-link" caret>
-            Barbara C.
-          </DropdownToggle>
+        <UserName>John Doe</UserName>
+          <DropdownToggle tag="a" className="nav-link" caret></DropdownToggle>
           <DropdownMenu>
-            <DropdownItem tag="a" href="/blah" active>
+            <DropdownItem tag="a" active>
+              Fechar Caixa
+            </DropdownItem>
+            <DropdownItem tag="a" active>
               Log-Off
             </DropdownItem>
           </DropdownMenu>
@@ -51,106 +206,115 @@ const CloseBox = () => {
         <ContainerVenda>
           <ContainerProducts>
             <ContainerBusca>
-              <HTitulo>Fluxo de Caixa </HTitulo>
+              <FormList>
+                <InputFormList
+                  onChange={e => searchProductHandler(e.target.value)}
+                />
+                <BtnList>Cancelar</BtnList>
+              </FormList>
             </ContainerBusca>
-            <DivTabela>
-              <TableHead>
-                <ContainerSpan>
-                  <SpanListTh>Produto</SpanListTh>
-                </ContainerSpan>
-                <ContainerSpan>
-                  <SpanListTh>Saída</SpanListTh>
-                </ContainerSpan>
-                <ContainerSpan>
-                  <SpanListTh>Estoque</SpanListTh>
-                </ContainerSpan>
-              </TableHead>
 
-              <TableRow>
-                <ContainerSpan>
-                  <SpanListTd>Shake de Banana</SpanListTd>
-                </ContainerSpan>
-                <ContainerSpan>
-                  <SpanListTd>5 Un.</SpanListTd>
-                </ContainerSpan>
-                <ContainerSpan>
-                  <SpanListTd>27 Un.</SpanListTd>
-                </ContainerSpan>
-              </TableRow>
+          <ContainerTh>
+            <Tr>
+              
+                <Whapper>
+                  <Th>Nome</Th>
+                </Whapper>
+                <WhapperCustomNumber>
+                  <Th>Vendido</Th>
+                </WhapperCustomNumber>
+                <WhapperCustomNumber>
+                  <Th>Estoque</Th>
+                </WhapperCustomNumber>
+                <WhapperCustomNumber>
+                  <Th>Desc</Th>
+                </WhapperCustomNumber>
+                <WhapperCustomNumber>
+                  <Th>Sub</Th>
+                </WhapperCustomNumber>
+                <WhapperCustomBtn />
+                <WhapperCustomBtn />
+              
+            </Tr>
+          </ContainerTh>
+            <TabelaBody>
+              <TBody>
+                {products.map((p: Product) => (
+                  <TrBody key={p.id}>
+                    <BoxCustomName><Td> {p.name}</Td></BoxCustomName>
+                    
+                    
+                    <BoxBodyNumbers><Td> {p.price_suggest} </Td> </BoxBodyNumbers>
+                                        
+                    <BoxBodyNumbers>
+                    <Td> {p.price_suggest} </Td>
+                    </BoxBodyNumbers>
+                    <BoxBodyNumbers>
+                    <Td> {p.price_suggest} </Td>
+                    </BoxBodyNumbers>
+                    <BoxBodyNumbers>
+                    <Td> {p.price_suggest} </Td>
+                    </BoxBodyNumbers>
+                    <TdCustomImage>
+                    <img src="/image/adicionar.svg" alt=""/>
+                    </TdCustomImage>
+                    <TdCustomImage>
+                    
+                      </TdCustomImage>
+                  </TrBody>                  
+                ))}
 
-              <TableRow>
-                <ContainerSpan>
-                  <SpanListTd>Cookie de amendoa</SpanListTd>
-                </ContainerSpan>
-                <ContainerSpan>
-                  <SpanListTd>8 Un.</SpanListTd>
-                </ContainerSpan>
-                <ContainerSpan>
-                  <SpanListTd>13 Un.</SpanListTd>
-                </ContainerSpan>
-              </TableRow>
 
-              <TableRow>
-                <ContainerSpan>
-                  <SpanListTd>Shake de Protein</SpanListTd>
-                </ContainerSpan>
-                <ContainerSpan>
-                  <SpanListTd>1 Un.</SpanListTd>
-                </ContainerSpan>
-                <ContainerSpan>
-                  <SpanListTd>5 Un.</SpanListTd>
-                </ContainerSpan>
-              </TableRow>
-            </DivTabela>
+                <DivBtnPreviusNext >
+                  <BtnPreviosNext onClick={previusPage}><img src="/image/left.svg" /> anterior</BtnPreviosNext>
+                  <BtnPreviosNext onClick={nextProducts}> Próximo <img src="/image/right.svg" /></BtnPreviosNext>
+                </DivBtnPreviusNext>
+
+
+              </TBody>
+            </TabelaBody>
           </ContainerProducts>
+
           <ContainerTotais>
-            <SpanDetalhes> Fechamento do dia </SpanDetalhes>
+            <BoxDetalhes>
+            <Detalhes> Detalhes Vendas ao Cliente </Detalhes>
+            </BoxDetalhes>
+            <ContainerValores>
+              <Label>Subtotal:</Label>
+              <Descricao>R$100,00</Descricao>
+            </ContainerValores>
+            <ContainerValores>
+              <Label>Descontos:</Label>
+              <Descricao>R$10,00</Descricao>
+            </ContainerValores>
+            
 
+             <ContainerValores>
+              <Label>Pontuação Total:</Label>
+              <Descricao>540</Descricao>
+            </ContainerValores>
+            
             <ContainerValores>
-              <ContainerSpanTotais>
-                <LabelSubtotalDesconto>N° de Vendas:</LabelSubtotalDesconto>
-              </ContainerSpanTotais>
-              <ContainerSpanTotais>
-                <FormSubtotalDesconto>5</FormSubtotalDesconto>
-              </ContainerSpanTotais>
+              <Label>Formas de Pagamento</Label>
             </ContainerValores>
             <ContainerValores>
-              <ContainerSpanTotais>
-                <LabelSubtotalDesconto>Carro Chefe:</LabelSubtotalDesconto>
-              </ContainerSpanTotais>
-              <ContainerSpanTotais>
-                <FormSubtotalDesconto>Cookie</FormSubtotalDesconto>
-              </ContainerSpanTotais>
+            <Label>Dinheiro:</Label>
+              <Descricao>540</Descricao>
+            </ContainerValores>
+            <ContainerValores>
+            <Label>Débito:</Label>
+              <Descricao>540</Descricao>
+            </ContainerValores>
+            <ContainerValores>
+            <Label>Crédito:</Label>
+              <Descricao>540</Descricao>
             </ContainerValores>
 
-            <ContainerValores>
-              <ContainerSpanTotais>
-                <LabelSubtotalDesconto>Subtotais:</LabelSubtotalDesconto>
-              </ContainerSpanTotais>
-              <ContainerSpanTotais>
-                <FormSubtotalDesconto>R$100,00</FormSubtotalDesconto>
-              </ContainerSpanTotais>
-            </ContainerValores>
-            <ContainerValores>
-              <ContainerSpanTotais>
-                <LabelSubtotalDesconto>Descontos:</LabelSubtotalDesconto>
-              </ContainerSpanTotais>
-              <ContainerSpanTotais>
-                <FormSubtotalDesconto>R$10,00</FormSubtotalDesconto>
-              </ContainerSpanTotais>
-            </ContainerValores>
             <ContainerValores>
               <LabelValorTotal>Valor Total:</LabelValorTotal>
               <FormTotal>R$90,00</FormTotal>
             </ContainerValores>
-            <ContainerValores>
-              <LabelValorTotal>Dinheiro:</LabelValorTotal>
-              <FormTotal>R$50,00</FormTotal>
-            </ContainerValores>
-            <ContainerValores>
-              <LabelValorTotal>Cartões:</LabelValorTotal>
-              <FormTotal>R$40,00</FormTotal>
-            </ContainerValores>
+
             <ContainerValores>
               <BtnFinalizar>Fechar Caixa</BtnFinalizar>
             </ContainerValores>
@@ -161,4 +325,4 @@ const CloseBox = () => {
   )
 }
 
-export default CloseBox
+export default ListProducts
