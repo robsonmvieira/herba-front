077403e98e89f7e3ddadaeaf88b9bd2a4d014cb5
Cleiton Oliveira <cleiton.logistica@outlook.com>
2020-10-24 23:01:23 -0300

    :bug: correção dos links em dashboard e identação

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -11,6 +11,8 @@ import {
   BoxCustom,
   DivImg
 } from '../../../styles/dashboard'
+  import Link from 'next/link'
+
 
 const dashAdmin = () => {
   return (
@@ -18,111 +20,141 @@ const dashAdmin = () => {
       <Menu />
       <Container>
         <ContainerGrid>
-          <Grid>
-            <IconsTags>
-              <BoxSpan>
-                <BoxCustom>
-                  <Title href={'/dashboard/produtos'}>Produtos</Title>
-                </BoxCustom>
-                <DivImg>
-                  <img src="/image/produtos.svg" alt="" />
-                </DivImg>
-              </BoxSpan>
-            </IconsTags>
-          </Grid>
-          <Grid>
-            <IconsTags>
-              <BoxSpan>
-                <BoxCustom>
-                  <Title href={'/dashboard/clientes'}>Clientes</Title>
-                </BoxCustom>
-                <DivImg>
-                  <img src="/image/clientes.svg" alt="" />
-                </DivImg>
-              </BoxSpan>
-            </IconsTags>
-          </Grid>
+        <Grid>
+          <IconsTags>
+            <BoxSpan>
+              <BoxCustom>
+                <Title >Produtos</Title>
+                <Link href="/dashboard/produtos">
+                 <a>
+                  <DivImg>
+                    <img src="/image/produtos.svg" alt="" />
+                  </DivImg>
+                 </a>
+                </Link>
+              </BoxCustom>
+            </BoxSpan>
+          </IconsTags>
+        </Grid>
+          
+        <Grid>
+          <IconsTags>
+            <BoxSpan>
+              <BoxCustom>
 
-          <Grid>
-            <IconsTags>
-              <BoxSpan>
-                <BoxCustom>
-                  <Title>Controles</Title>
-                </BoxCustom>
-                <DivImg>
-                  <img src="/image/estatistica.svg" alt="" />
-                </DivImg>
-              </BoxSpan>
-            </IconsTags>
-          </Grid>
-        </ContainerGrid>
+                <Title >Clientes</Title>
+                <Link href="/dashboard/clientes">
+                 <a>
+                  <DivImg>
+                    <img src="/image/clientes.svg" alt="" />
+                  </DivImg>
+                 </a>
+                </Link>
+              </BoxCustom>
+            </BoxSpan>
+          </IconsTags>
+        </Grid>
 
-        <ContainerGrid>
-          <Grid>
-            <IconsTags>
-              <BoxSpan>
-                <BoxCustom>
-                  <Title>Perfil</Title>
-                </BoxCustom>
-                <DivImg>
-                  <img src="/image/admin.svg" alt="" />
-                </DivImg>
-              </BoxSpan>
-            </IconsTags>
-          </Grid>
+        <Grid>
+          <IconsTags>
+            <BoxSpan>
+              <BoxCustom>
+                <Title >Controles</Title>
+                <Link href="/">
+                 <a>
+                  <DivImg>
+                    <img src="/image/estatistica.svg" alt="" />
+                  </DivImg>
+                 </a>
+                </Link>
+              </BoxCustom>
+            </BoxSpan>
+          </IconsTags>
+        </Grid>
+      </ContainerGrid>
 
-          <Grid>
-            <IconsTags>
-              <BoxSpan>
-                <BoxCustom>
-                  <Title href={'/pdv'}>PDV</Title>
-                </BoxCustom>
-                <DivImg>
-                  <img src="/image/codebar.svg" alt="" />
-                </DivImg>
-              </BoxSpan>
-            </IconsTags>
-          </Grid>
+      <ContainerGrid>
+        <Grid>
+          <IconsTags>
+            <BoxSpan>
+              <BoxCustom>
+                <Title >Configurações</Title>
+                <Link href="/configuracao">
+                 <a>
+                  <DivImg>
+                    <img src="/image/admin.svg" alt="" />
+                  </DivImg>
+                 </a>
+                </Link>
+              </BoxCustom>
+            </BoxSpan>
+          </IconsTags>
+        </Grid>
 
-          <Grid>
-            <IconsTags>
-              <BoxSpan>
-                <BoxCustom>
-                  <Title href={'/pdd'}>PDD</Title>
-                </BoxCustom>
-                <DivImg>
-                  <img src="/image/closebox.svg" alt="" />
-                </DivImg>
-              </BoxSpan>
-            </IconsTags>
-          </Grid>
-        </ContainerGrid>
-        <ContainerGrid>
-          <Grid>
-            <IconsTags>
-              <BoxSpan>
-                <Title></Title>
-              </BoxSpan>
-            </IconsTags>
-          </Grid>
+        <Grid>
+          <IconsTags>
+            <BoxSpan>
+              <BoxCustom>
+                <Title >PDV</Title>
+                <Link href="/pdv">
+                 <a>
+                  <DivImg>
+                    <img src="/image/codebar.svg" alt="" />
+                  </DivImg>
+                 </a>
+                </Link>
+              </BoxCustom>
+            </BoxSpan>
+          </IconsTags>
+        </Grid>
 
-          <Grid>
-            <IconsTags>
-              <BoxSpan>
-                <Title></Title>
-              </BoxSpan>
-            </IconsTags>
-          </Grid>
+        <Grid>
+          <IconsTags>
+            <BoxSpan>
+              <BoxCustom>
+                <Title >PDD</Title>
+                <Link href="/pdd">
+                 <a>
+                  <DivImg>
+                    <img src="/image/closebox.svg" alt="" />
+                  </DivImg>
+                 </a>
+                </Link>
+              </BoxCustom>
+            </BoxSpan>
+          </IconsTags>
+        </Grid>
+      </ContainerGrid>
 
-          <Grid>
-            <IconsTags>
-              <BoxSpan>
-                <Title></Title>
-              </BoxSpan>
-            </IconsTags>
-          </Grid>
-        </ContainerGrid>
-      </Container>
+
+
+      <ContainerGrid>
+        <Grid>
+          <IconsTags>
+            <BoxSpan>
+              
+            </BoxSpan>
+          </IconsTags>
+        </Grid>
+
+        <Grid>
+          <IconsTags>
+            <BoxSpan>
+              
+            </BoxSpan>
+          </IconsTags>
+        </Grid>
+
+        <Grid>
+          <IconsTags>
+            <BoxSpan>
+              
+            </BoxSpan>
+          </IconsTags>
+        </Grid>
+
+      </ContainerGrid>
+    </Container>
     </>
   )
 }
